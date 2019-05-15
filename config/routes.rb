54c6778aee4b69ad695207cf5078529e9c6ccd2f
alemat <alematgebru@gmail.com>
2019-05-15 15:04:39 +0300

    first lwt commit

---
+++ config/routes.rb
@@ -0,0 +1,88 @@
+Rails.application.routes.draw do
+  resources :training_funds do
+    collection do
+      get 'funding_by_area'
+    end
+  end
+  resources :languages
+  resources :professions
+  resources :profession_categories
+  get 'reports/trainings'
+  post 'reports/trainings'
+  get 'reports/health_workers'
+  post 'reports/health_workers'
+  get 'reports/trainees'
+  post 'reports/trainees'
+
+  resources :trainees do
+    collection do
+      get 'load_trainees'
+      get 'institution_trainees_by_area'
+      get 'institution_trainees_by_region'
+      get 'institution_trainees_by_year'
+
+      get 'org_unit_trainees_by_area'
+      get 'org_unit_trainees_by_year'
+      get 'org_unit_trainees_by_category'
+    end
+  end
+  resources :people do
+    collection do
+      get 'load_people'
+      get 'load_professions'
+    end
+  end
+  resources :facilities do
+    collection do
+      get 'load_facilities'
+    end
+  end
+  resources :facility_types
+  resources :trainings do
+    collection do
+      get 'load_distribution'
+    end
+    member do
+      delete 'cancel'
+      get 'undo_cancel'
+    end
+  end
+
+  resources :training_titles do
+    collection do
+      get 'training_titles_by_area'
+      get 'load_training_titles'
+    end
+  end
+
+  resources :institutions
+  resources :institution_types
+  resources :organization_units do
+    collection do
+      get 'load_tree'
+      get 'load_sub_units'
+      get 'org_unit_facilities'
+    end
+  end
+  resources :organization_types
+  resources :roles
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+  scope "/admin" do
+    resources :users do
+      collection do
+        get 'load_users'
+        get 'institution_users'
+        get 'new_institution_user'
+        get 'edit_institution_user'
+      end
+    end
+  end
+  get 'home/index'
+  root to: "home#index"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
